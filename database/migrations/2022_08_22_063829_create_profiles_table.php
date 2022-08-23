cca84d1ce7c386317fadd165fc695326928d4eab
your username <s.amirreza1376ardebili@gmail.com>
2022-08-23 16:50:01 +0430

    first

---
+++ database/migrations/2022_08_22_063829_create_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone',11);
+            $table->string('mobile')->nullable();
+            $table->longText('about_me');
+            $table->bigInteger('number_project');
+            $table->bigInteger('profit');
+            $table->bigInteger('Growth');
+            $table->bigInteger('Fund');
+            $table->foreignIdFor(file::class);
+            $table->softdeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
